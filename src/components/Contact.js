a9c2ad816d55564b2b978821896514da77a46172
rofiqirapsanjani <achmad.rofiqi@ui.ac.id>
2022-09-28 20:20:10 +0700

    second commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ import {
   
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
-const Contact = (props) => {
+const Contact = (data) => {
     // Contact berisi foto, nama, telepon, dan email
     return (
     <>
@@ -33,7 +33,7 @@ const Contact = (props) => {
       </TableHead>
       <TableBody>
         {/* Asumsi nama props adalah todos */}
-        {props.contacts.map((todo) => (
+        {data.contacts.map((todo) => (
           <TableRow key={todo.id}>
             <TableCell>{todo.name}</TableCell>
             <TableCell>{todo.phone}</TableCell>
